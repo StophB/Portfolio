097d15d1aacd9f94a3912604ffa74dc23b3e5d77
mustapha bousfina <bousfinamustapha@gmail.com>
2023-09-18 10:56:03 +0100

    updated

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -3,6 +3,8 @@ import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
 import chatify from "../../Assets/Projects/chatify.png";
+import planetphone from "../../Assets/Projects/planetphone.png";
+import admin from "../../Assets/Projects/admin.png";
 
 function Projects() {
   return (
@@ -24,6 +26,17 @@ function Projects() {
               description="Personal Chat Room or Workspace to share resources and hangout with friends build with react.js, Material-UI, and Firebase. Have features which allows user for realtime messaging, image sharing as well as supports reactions on messages."
               ghLink="https://github.com/soumyajit4419/Chatify"
               demoLink="https://chatify-49.web.app/"
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={planetphone}
+              isBlog={false}
+              title="PlanetPhone"
+              description="PlanetPhone is a dynamic e-commerce platform I personally developed using Laravel and Livewire. Specializing in cutting-edge mobile phones and accessories, it offers users a seamless shopping experience with a focus on innovation, user-friendliness, and security."
+              ghLink="https://github.com/StophB/planetphone"
+              demoLink="https://github.com/StophB/planetphone"
             />
           </Col>
 
